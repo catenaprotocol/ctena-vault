bdd5de969978ca4076934e2480b0cc2201bf0ced
scobru <catenaprotocol@gmail.com>
2021-10-05 17:14:14 +0200

    commit

--- src/Root.js
+++ src/Root.js
@@ -61,7 +61,7 @@ function Root() {
         <meta name="description" content={getPageMeta('App-Meta-Description')} />
         <meta property="og:title" content={getPageMeta('App-Meta-Title')} />
         <meta property="og:description" content={getPageMeta('App-Meta-Description')} />
-        <meta property="og:url" content={process.env.PUBLIC_URL || 'https://ribbon.ctena.finance'} />
+        <meta property="og:url" content={process.env.PUBLIC_URL || 'https://vault.ctena.finance'} />
       </Helmet>
       <HashRouter>{children}</HashRouter>
     </Provider>
