80d26e3314d17d6fac2b9d80dd5b3b22990de547
catenaprotocol <catenaprotocol@gmail.com>
2022-01-01 18:18:09 +0100

    add beltBTC Vault

--- src/features/configure/index.js
+++ src/features/configure/index.js
@@ -29,6 +29,7 @@ export {
   beefyUniV2ZapABI,
   uniswapV2PairABI,
   uniswapV2RouterABI,
+  masterchefABI,
 } from './abi';
 export { bscStakePools } from './stake/bsc_stake';
 export { hecoStakePools } from './stake/heco_stake';
